f296e4aa28c4d2eea54f992dd55eaaffb10d7a79
Elsterka <38614398+Elsterka@users.noreply.github.com>
2025-03-16 16:32:18 -0600

    Update and rename 1.h to App.js

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "./styles/globals/reset.css";
+import "./styles/globals/colors.css";
+import "./styles/globals/typography";
+import "./styles/globals/util.css";
+import "./styles/globals/button.css";
